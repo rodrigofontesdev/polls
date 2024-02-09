44499542b80b38451bb4fa03c8713719d8136f01
Rodrigo Fontes <rodrigo_fnts@hotmail.com>
2024-02-08 21:54:06 -0300

    initial commit

---
+++ prisma/migrations/20240208004632_create_poll_options_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "PollOptions" (
+    "id" TEXT NOT NULL,
+    "pollsId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+
+    CONSTRAINT "PollOptions_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "PollOptions" ADD CONSTRAINT "PollOptions_pollsId_fkey" FOREIGN KEY ("pollsId") REFERENCES "Polls"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
